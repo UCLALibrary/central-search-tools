2cce09892fac7d94f20ba495fc1dce3dd3671fe0
Zoe Tucker <zoetucker@library.ucla.edu>
2024-07-08 17:11:44 -0700

    SYS-1646: add config file for sheet music

---
+++ config/sheet_music.py
@@ -0,0 +1,31 @@
+SOURCE_QUERY = "*:*"
+
+
+def get_id(record: dict) -> str:
+    return record.get("id")
+
+
+def map_record(record: dict) -> dict:
+
+    # only take a selection of fields for now
+    fields_to_keep = [
+        "id",
+        "titles",
+        "publishers",
+        "subjects",
+        "names",
+    ]
+    output_record = {}
+    for fld in fields_to_keep:
+        if fld in record:
+            output_record[fld] = record[fld]
+
+    # URL for MODS record isn't in original metadata, but we can construct it
+    output_record["mods_url"] = (
+        "https://static.library.ucla.edu/sheetmusic/mods/"
+        + f"{record['collectionKey']}/{record['id']}.xml"
+    )
+    # add new field for source
+    output_record["source"] = "Sheet Music"
+
+    return output_record
