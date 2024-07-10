bd8bacdda4b70d0236fed6e8b6fd2e29476d93a7
Zoe Tucker <zoetucker@library.ucla.edu>
2024-07-10 11:43:41 -0700

    SYS-1649: Copy PRL Solr data to Elasticsearch

---
+++ config/PRL.py
@@ -0,0 +1,32 @@
+SOURCE_QUERY = "*:*"
+
+
+def get_id(record: dict) -> str:
+    return record.get("id")
+
+
+def map_record(record: dict) -> dict:
+
+    # only take a selection of fields for now
+    # rename fields for consistency
+    fields_to_keep = {
+        "id": "id",
+        "title_keyword": "titles",
+        "description_keyword": "descriptions",
+        "publisher_keyword": "publishers",
+        "subject_keyword": "subjects",
+        "creator_keyword": "creators",
+        "contributor_keyword": "contributors",
+        "type_keyword": "types",
+        "external_link": "url",
+    }
+
+    output_record = {}
+    for fld in fields_to_keep.keys():
+        if fld in record:
+            output_record[fields_to_keep[fld]] = record[fld]
+
+    # add new field for source
+    output_record["source"] = "PRL"
+
+    return output_record
