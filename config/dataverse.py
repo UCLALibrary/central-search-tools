4ec586dcbcc13bbd0bd961f3177b1f8eddbcff8d
Andy Kohler <akohler@library.ucla.edu>
2024-07-16 08:23:28 -0800

    Add specific field mapping for dataverse

--- config/dataverse.py
+++ config/dataverse.py
@@ -2,8 +2,31 @@ SOURCE_QUERY = "*"
 
 
 def get_id(record: dict) -> str:
+    # Only type=dataverse seems to have reliable "identifier" field;
+    # use url field for now.
     return record.get("url")
 
 
 def map_record(record: dict) -> dict:
-    return record
+    # Only take a selection of fields for now;
+    # rename fields for consistency.
+    fields_to_keep = {
+        "id": "id",
+        "type": "type",
+        "url": "url",
+        "name": "title",
+        "publisher": "publisher",
+        "description": "description",
+        "subjects": "subjects",
+        "keywords": "keywords",
+    }
+
+    output_record = {}
+    for fld in fields_to_keep.keys():
+        if fld in record:
+            output_record[fields_to_keep[fld]] = record[fld]
+
+    # Add new field for source
+    output_record["source"] = "Dataverse"
+
+    return output_record
